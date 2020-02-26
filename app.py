a2c0bd8a1214785881a820e7e24ad31a025ac0a3
JoshuaBruens <josh.bruens@gmail.com>
2020-02-26 17:42:09 +0100

    New Page

--- app.py
+++ app.py
@@ -21,8 +21,6 @@ app.config['attributes'] = pd.read_csv("./static/attributes/NOWHERE_DATASET.csv"
 def index():
 
 
-
-
     if request.method == "POST":
         app.config['image_name'] = request.form["content"]
         return redirect('/')
@@ -34,13 +32,26 @@ def index():
         full_filename = os.path.join(PEOPLE_FOLDER , picture_filename)
 
         image_list = app.config['image_list']
-
         db_attributes = app.config['attributes']
 
-
-
-
         return render_template('index.html', image = image_name ,user_image = full_filename, image_list = image_list )
+
+@app.route('/graph', methods=['POST','GET'])
+def graph():
+
+    if request.method == 'POST':
+        try :
+            return  redirect('/')
+
+        except :
+            return "There was an issue updating your task"
+
+    else :
+        return render_template('graph.html')
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug = True)
